f0d42fe4af3b57f4707c0d4654ed83edbb9e8b0f
TheDao032 <nthedao2705@gmail.com>
2024-08-19 14:35:56 +0700

    build: update roles for repmgr

---
+++ ansible/playbooks/roles/postgresql/repmgr/repmgr-configuration/files/repmgr-common.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -e
+PSQL_VERSION=$1
+PSQL_PORT=$2
+PSQL_CONFIG_PATH=/etc/postgresql/${PSQL_VERSION}/main/postgresql.conf
+
+declare -A psql_conf=(
+ [#listen_addresses]="listen_addresses = '*'"
+ [#max_wal_senders]='max_wal_senders = 10'
+ [#max_replication_slots]='max_replication_slots = 10'
+ [#hot_standby]='hot_standby = on'
+ [#wal_log_hints]='wal_log_hints = on'
+ [#archive_mode]='archive_mode = on'
+ [#wal_level]='wal_level = hot_standby'
+ [#log_statement]="log_statement = 'all'"
+ [#archive_command]="archive_command = '/bin/true'"
+ [shared_preload_libraries]="shared_preload_libraries = 'citus,repmgr'"
+)
+
+for key in "${!psql_conf[@]}"; do
+  key="${key}"
+  value="${psql_conf[${key}]}"
+  sed -i "/${key}/c\\${value}" ${PSQL_CONFIG_PATH}
+done
+
+su -c "psql -p ${PSQL_PORT} -f ${PSQL_REPMGR_SQL_PATH}" postgres
+
+exit 0
---
+++ ansible/playbooks/roles/postgresql/repmgr/repmgr-primary-conf/files/setup-repmgr.sql
@@ -0,0 +1,13 @@
+DO $$
+BEGIN
+    -- Check if the user "repmgr" exists, and create it with SUPERUSER privileges if it doesn't exist.
+    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'repmgr') THEN
+        CREATE ROLE repmgr WITH SUPERUSER CREATEROLE CREATEDB LOGIN;
+    END IF;
+END
+$$;
+
+-- Check if the database "repmgr" exists, and create it with "repmgr" as the owner if it doesn't exist.
+\c postgres
+SELECT 'CREATE DATABASE repmgr OWNER repmgr' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'repmgr')\gexec
+-- SELECT 'CREATE EXTENSION repmgr' WHERE NOT EXISTS (SELECT 1 FROM pg_extension WHERE extname = 'repmgr');\gexec
