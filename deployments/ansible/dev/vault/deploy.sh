59ebba345ef3c38ebea61f02baa4867f81203d90
TheDao032 <nthedao2705@gmail.com>
2025-04-05 11:44:56 +0700

    build: update gitignore and update deployments

--- deployments/ansible/dev/vault/deploy.sh
+++ deployments/ansible/dev/vault/deploy.sh
@@ -2,8 +2,15 @@
 
 set -e
 
-ANSIBLE_ENV=${ANSIBLE_ENV:-"dev"}
 SERVICE=${SERVICE:-"vault"}
+SCRIPT_ABS_PATH="$( realpath "${0}")"
+LIB_DIR="${SCRIPT_ABS_PATH%/*}/../env-vars/${SERVICE}"
+
+for LIB_FILE in "${LIB_DIR}"/*.bash; do
+  source "${LIB_FILE}" || { log_info "$(date -u) - FATAL - failure occured while reading ${LIB_FILE}"; exit 1; }
+done
+
+ENVIRONMENT=${ENVIRONMENT:-"dev"}
 PROVIDER=${PROVIDER:-"virtualbox"}
 UTILS_SCRIPT="${UTILS_SCRIPT:-"deployments/utils/setup_env.sh"}"
 
@@ -14,24 +21,24 @@ ANSIBLE_DIR=${ANSIBLE_DIR:-${DEVOPS_TOOLS_DIR}/ansible}
 ANSIBLE_PLAYBOOKS_DIR=${ANSIBLE_DIR}/playbooks/vault-playbooks
 ANSIBLE_INVENTORIES_DIR=${ANSIBLE_DIR}/inventories
 VAGRANTFILE="vagrant-files/kubernetes/k3s.${PROVIDER}.Vagrantfile"
-INVENTORY=${ANSIBLE_INVENTORIES_DIR}/${ANSIBLE_ENV}/${SERVICE}/${PROVIDER}
+INVENTORY=${ANSIBLE_INVENTORIES_DIR}/${ENVIRONMENT}/${SERVICE}/${PROVIDER}
 
 NETWORK_MODE=${NETWORK_MODE:-"NAT"} VBOX_GUEST_DISK=${VBOX_GUEST_DISK:-"/Applications/VirtualBox.app/Contents/MacOS/VBoxGuestAdditions.iso"}
 
 source ${DEVOPS_TOOLS_DIR}/${UTILS_SCRIPT} || { log_info "$(date -u) - FATAL - failure occured while reading ${LIB_FILE}"; exit 1; }
 
-# LIB_FILE=${DEVOPS_TOOLS_DIR}/deployments/ansible/${ANSIBLE_ENV}/env-vars/k3s-env.bash
+# LIB_FILE=${DEVOPS_TOOLS_DIR}/deployments/ansible/${ENVIRONMENT}/env-vars/k3s-env.bash
 # source "${LIB_FILE}" || { log_info "$(date -u) - FATAL - failure occured while reading ${LIB_FILE}"; exit 1; }
 
 RHEL_USERNAME=${1:-""}
 RHEL_PASSWORD=${2:-""}
 
-SCRIPT_ABS_PATH="$( realpath "${0}")"
-LIB_DIR="${SCRIPT_ABS_PATH%/*}/../env-vars/${SERVICE}"
-
-for LIB_FILE in "${LIB_DIR}"/*.bash; do
-  source "${LIB_FILE}" || { log_info "$(date -u) - FATAL - failure occured while reading ${LIB_FILE}"; exit 1; }
-done
+# SCRIPT_ABS_PATH="$( realpath "${0}")"
+# LIB_DIR="${SCRIPT_ABS_PATH%/*}/../env-vars/${SERVICE}"
+#
+# for LIB_FILE in "${LIB_DIR}"/*.bash; do
+#   source "${LIB_FILE}" || { log_info "$(date -u) - FATAL - failure occured while reading ${LIB_FILE}"; exit 1; }
+# done
 
 declare vagrant_plugins=(
   "vagrant-vbguest"
@@ -60,7 +67,7 @@ vagrant_init() {
 }
 
 ansible_exec() {
-  uv run ansible/inventories/${ANSIBLE_ENV}/${SERVICE}/${PROVIDER}/dynamic_inventory.py --list
+  uv run ansible/inventories/${ENVIRONMENT}/${SERVICE}/${PROVIDER}/dynamic_inventory.py --list
 
   log_info "Running setup vault dependencies packages"
   ansible-playbook ${ANSIBLE_PLAYBOOKS_DIR}/dependencies/main.yml -i ${INVENTORY} -vvv
